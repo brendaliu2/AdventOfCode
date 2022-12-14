deaf98fbdf2162091e30456a9a5c383ff562b2e3
Brenda Liu <brenda.liu24@gmail.com>
2022-12-13 22:29:04 -0600

    add tailwind

---
+++ src/days/one/one.tsx
@@ -0,0 +1,9 @@
+import { input } from "./oneInput";
+
+export default function One() {
+    return (
+        <div className="container mx-auto m-4 p-4 border border-zinc-500">
+            Card 1
+        </div>
+    );
+}
